b523cb91695f61582112352a3b9ed7ed54a06150
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2023-10-11 01:28:27 -0500

    Inventory Panel UI
    Rough draft of the Inventory Panel UI

---
+++ Day At The Apothecary/Assets/Scripts/Inventory/InventoryManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        //If there is more than on instance, destroy the extra
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            //Set the static instance to this instance
+            Instance = this;
+        }
+    }
+
+    //Item Slots
+    public ItemData[] items = new ItemData[8];
+
+    //Item in the player's hand
+    public ItemData equippedItem = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
