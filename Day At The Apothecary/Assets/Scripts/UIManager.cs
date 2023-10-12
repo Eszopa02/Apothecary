87bb3f87fedd4f71f84b18d8deea2b548d8a6d1d
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2023-10-11 22:13:54 -0500

    Updated UI Manager with Render Inventory Script
    Having an issue with code for RenderAnimation()

--- Day At The Apothecary/Assets/Scripts/UIManager.cs
+++ Day At The Apothecary/Assets/Scripts/UIManager.cs
@@ -7,6 +7,12 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager Instance { get; private set; }
 
+    //The inventory panle
+    public GameObject inventoryPanel;
+
+    //The item slot UIs
+    public InventorySlot[] itemSlots;
+
     private void Awake()
     {
         //If there is more than one isntance, destroy the extra
@@ -24,7 +30,35 @@ public class UIManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        RenderInventory();
+    }
+
+    //Render the inventory screen to reflect the Player's Inventory. 
+    public void RenderInventory()
+    {
+        //Get the inventory item slots from Inventory Manager
+        ItemData[] inventoryItemSlots = InventoryManager.Instance.items;
+
+        //Render the item section
+        RenderInventoryPanel(inventoryItemSlots, itemSlots);
+    }
+
+    //Iterate through a slot in a section and display them in the UI
+    void RenderInventoryPanel(ItemData[] slots, InventorySlot[] uiSlots)
+    {
+        for (int i = 0; i < uiSlots.Length; i++)
+        {
+            //Display them accordingly
+            uiSlots[i].Display(slots[i]);
+        }
+    }
+
+    public void ToggleInventoryPanel()
+    {
+        //If the panel is hidden, show it and vice versa
+        inventoryPanel.SetActive(!inventoryPanel.activeSelf);
+
+        RenderInventory();
     }
 
    
