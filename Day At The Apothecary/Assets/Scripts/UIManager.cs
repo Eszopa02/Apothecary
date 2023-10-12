1612eca9f10d22a150a08859b9f9ae18d9c10cb8
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2023-10-11 21:48:16 -0500

    Created UI Manager script

---
+++ Day At The Apothecary/Assets/Scripts/UIManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        //If there is more than one isntance, destroy the extra
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            //Set the static instance to this instance
+            Instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+}
