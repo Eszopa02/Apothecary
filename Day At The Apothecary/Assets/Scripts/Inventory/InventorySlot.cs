e022fab9d6ab50f3576aa043ff2aa7bb9ff07181
Eden Szopa <71853521+Eszopa02@users.noreply.github.com>
2023-10-11 21:57:48 -0500

    Created Inventory Slot script and attached it to the game object

---
+++ Day At The Apothecary/Assets/Scripts/Inventory/InventorySlot.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlot : MonoBehaviour
+{
+    ItemData itemToDisplay;
+
+    public Image itemDisplayImage;
+
+    public void Display(ItemData itemToDisplay)
+    {
+        //Check if there is an item to display
+        if (itemToDisplay != null)
+        {
+            //Switch the thumbnail over
+            itemDisplayImage.sprite = itemToDisplay.thumbnail;
+            this.itemToDisplay = itemToDisplay;
+        }
+    }
+}
